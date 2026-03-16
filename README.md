9098a4e91688f90c0b41624b877ebd6007f52216
Ranga Swamy <crswamy929@gmail.com>
2026-03-16 14:02:37 +0530

    second changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pyspark-Data-Profiling-
 Data Profling while ingesting the data 
+![Alt text](<Screenshot 2026-03-15 171342.png>)
---
+++ dataprofiling.py
@@ -0,0 +1,5 @@
+import sys
+import datetime, time
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
